ea62593615d25d2d985e592548297f93729a707c
andrej fritz <a.fritz@betzold.de>
2014-09-10 16:43:45 +0200

    updated to meteor 0.9

--- package.js
+++ package.js
@@ -1,5 +1,7 @@
 Package.describe({
-  summary: 'Simple PNG encoder/decoder for Node.js with no native dependencies.'
+  summary: 'Simple PNG encoder/decoder for Node.js with no native dependencies.',
+  version: "0.0.2",
+  git: "https://github.com/andruschka/meteor-png.git"
 });
 
 Npm.depends({
@@ -7,6 +9,7 @@ Npm.depends({
 });
 
 Package.on_use(function(api) {
+  api.versionsFrom("METEOR@0.9.0");
   api.add_files('include-png.js', ['server']);
   api.export('PNG', 'server');
 });
